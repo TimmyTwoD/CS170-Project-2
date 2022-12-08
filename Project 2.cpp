a18ec5e51658899c9462125ccdb718d2247d0c61
TimmyTwoD <david12345l@gmail.com>
2022-12-08 04:02:04 -0800

    input txt file values into vector

--- Project 2.cpp
+++ Project 2.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main(){
 
-    //testing start
-    cout << "Hello World"<<endl;
+    vector<vector<double>> dataSet;
+    ifstream myfile("CS170_Small_Data__96.txt"); 
+    string content;
+    double value; //change all doubles to floats to see if it still runs accurately and runs faster
+
+    while(getline(myfile, content)){
+        stringstream row(content); 
+        vector<double> temp;
+        while(row >> value){
+            temp.push_back(value);
+        }
+        dataSet.push_back(temp);
+    }
+
+    myfile.close();
+
+    /*
+    for (int i = 0; i < dataSet.size(); i++)
+    {
+        for (int j = 0; j < dataSet[i].size(); j++)
+        {
+            cout << dataSet[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+    */
 
     return 0;
 }
